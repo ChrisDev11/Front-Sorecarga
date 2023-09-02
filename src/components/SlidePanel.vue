dc09d1bd98765a4f7e261244ae084cca468e5e1a
Christian <chrispedro.dev11@gmail.com>
2023-09-02 17:33:43 -0300

    Atualização 02-09

---
+++ src/components/SlidePanel.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="panel-wrap" :class="{ visible: visible}">
+    <div class="panel">
+        <div>
+            <button class="btn btn-danger"
+            @click="closePanel()"
+            >
+            Fechar
+        </button>
+        <h2>{{ title }}</h2>
+        </div>
+        <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SlidePanel',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    visible: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  emits: ['update:visible'],
+  methods: {
+    closePanel() {
+      this.$emit('update:visible', false);
+    },
+  },
+};
+</script>
+
+
+
+<style lang="scss" scoped>
+.panel {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: #333;
+  color: #eee;
+  overflow: auto;
+  padding: 1em;
+
+  &-wrap {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    width: 30em;
+    transform: translateX(100%);
+    transition: 0.3s ease-out;
+
+    &.visible {
+      transform: translateX(0);
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,24 +4,42 @@
       <!-- Conteúdo do banner -->
       <h1>Bem-vindo à nossa loja!</h1>
       <p>Encontre os melhores produtos aqui.</p>
+      <button class="btn btn-primary" @click="changeCartVisibility()">Exibir Carrinho</button>
+
+      <SlidePanel
+        title = "Cart"
+        :visible="cartVisible"
+        @update:visible="changeCartVisibility"
+      ></SlidePanel>
+
     </div>
 
     <div class="container">
       <h2>Nossos Produtos</h2>
       <div class="product-list">
         <!-- Loop para exibir a lista de produtos -->
-        <router-link v-for="product in products" :key="product.id" :to="`/product/${product.id}`" class="product-card">
-  <div class="product-card-content">
-    <img :src="product.image" :alt="product.name" class="product-image">
-    <h3 class="product-name">{{ product.name }}</h3>
-    <p class="product-description">{{ product.description }}</p>
-    <p class="product-price">R$ {{ product.price }}</p>
-    <div class="product-buttons">
-      <button class="buy-button">Comprar</button>
-      <button class="add-to-cart-button">Adicionar ao Carrinho</button>
-    </div>
-  </div>
-</router-link>
+        <div
+          v-for="product in products"
+          :key="product.id"
+          :to="`/product/${product.id}`"
+          class="product-card"
+        >
+          <img :src="product.image" :alt="product.name" class="product-image" />
+          <h3>{{ product.name }}</h3>
+          <p class="product-description">{{ product.description }}</p>
+          <p>R$ {{ product.price }}</p>
+          <div class="product-buttons">
+            <button @click.stop="buyProduct(product.id)" class="buy-button">
+              Comprar
+            </button>
+            <button
+              @click.stop="addToCart(product.id)"
+              class="add-to-cart-button"
+            >
+              Adicionar ao Carrinho
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -29,28 +47,58 @@
 
 <script>
 import axios from "axios";
+import SlidePanel from "@/components/SlidePanel.vue"
+import { ref } from "vue";
+// TEste Carrinho
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////
 
 export default {
+  components:{
+    SlidePanel
+  },
+
+// Exibir Carinho teste
+  setup(){
+    const cartVisible = ref(false);
+
+    const changeCartVisibility = () => {
+      cartVisible.value = !cartVisible.value;
+      console.log(cartVisible.value);
+    };
+
+    return {
+      cartVisible,
+      changeCartVisibility,
+    };
+  },
+////////////////////////////////
   data() {
     return {
-      products: []
+      products: [],
     };
   },
   mounted() {
     this.fetchProducts();
   },
   methods: {
+  
     fetchProducts() {
       axios
         .get("https://localhost:3000/Product")
-        .then(response => {
+        .then((response) => {
           this.products = response.data;
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(error);
         });
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -65,40 +113,34 @@ export default {
   margin-top: 20px;
 }
 
-.product-list {
-  display: flex;
-  flex-wrap: wrap;
-}
-
 .product-card {
   text-decoration: none;
-  display: block;
+  display: inline-block;
   width: 300px;
-  margin: 20px;
+  margin: 10px;
+  padding: 10px;
   border: 1px solid #ccc;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
   transition: transform 0.2s ease-in-out;
+  background-color: #fff;
 }
 
 .product-card:hover {
   transform: translateY(-5px);
 }
 
-/* Estilo para o conteúdo do card de produto */
-.product-card-content {
-  padding: 20px;
-}
-
 /* Estilo para a imagem do produto */
 .product-image {
   max-width: 100%;
   height: auto;
+  border-bottom: 1px solid #ccc;
 }
 
 /* Estilo para o nome do produto */
-.product-name {
+.product-card h3 {
   font-size: 18px;
   margin-top: 10px;
+  text-align: center;
 }
 
 /* Estilo para a descrição do produto */
@@ -106,6 +148,7 @@ export default {
   font-size: 14px;
   color: #555;
   margin-top: 8px;
+  text-align: center;
 }
 
 /* Estilo para o preço do produto */
@@ -114,12 +157,13 @@ export default {
   font-weight: bold;
   color: #e67e22;
   margin-top: 10px;
+  text-align: center;
 }
 
 /* Estilo para os botões de ação */
 .product-buttons {
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   margin-top: 20px;
 }
 
@@ -128,13 +172,16 @@ export default {
   padding: 10px 15px;
   font-size: 14px;
   cursor: pointer;
+  border: none;
+  border-radius: 4px;
+  width: 120px;
+  text-align: center;
 }
 
 .buy-button {
   background-color: #e67e22;
   color: #fff;
-  border: none;
-  border-radius: 4px;
+  margin-right: 10px;
   transition: background-color 0.2s ease-in-out;
 }
 
@@ -145,8 +192,6 @@ export default {
 .add-to-cart-button {
   background-color: #3498db;
   color: #fff;
-  border: none;
-  border-radius: 4px;
   transition: background-color 0.2s ease-in-out;
 }
 
