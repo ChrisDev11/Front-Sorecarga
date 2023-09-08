e5a6f6f935a79c91c2b40f82a3184a40c39e7cca
Christian <chrispedro.dev11@gmail.com>
2023-09-08 16:50:22 -0300

    Compras 50%

--- src/components/CheckOut.vue
+++ src/components/CheckOut.vue
@@ -32,7 +32,7 @@
         Total:
         <span class="green-text">R$ {{ calculateTotal().toFixed(2) }}</span>
       </div>
-      <button @click="nextStep">Prosseguir Compra</button>
+      <button @click="createPurchase()">Prosseguir Compra</button>
     </div>
     <div v-else-if="step === 2">
       <!-- Etapa 2: Dados do Cliente -->
@@ -40,7 +40,7 @@
       <form>
         <div class="form-group">
           <label for="nome">Nome:</label>
-          <input type="text" id="nome" v-model="cliente.nome" />
+          <input type="text" id="nome" v-model="cliente.name" />
         </div>
         <div class="form-group">
           <label for="cpf">CPF:</label>
@@ -48,7 +48,7 @@
         </div>
         <div class="form-group">
           <label for="telefone">Telefone:</label>
-          <input type="text" id="telefone" v-model="cliente.telefone" />
+          <input type="text" id="telefone" v-model="cliente.phone" />
         </div>
         <div class="form-group">
           <label for="email">Email:</label>
@@ -56,13 +56,15 @@
         </div>
         <div class="form-group">
           <label for="formaPagamento">Forma de Pagamento:</label>
-          <select id="formaPagamento" v-model="cliente.formaPagamento">
+          <select id="formaPagamento" required>
             <option value="pix">PIX</option>
           </select>
         </div>
       </form>
       <button @click="prevStep">Voltar</button>
-      <button @click="generatePayloadAndProceedToNextStep">Gerar Pagamento</button>
+      <button @click="generatePayloadAndProceedToNextStep">
+        Gerar Pagamento
+      </button>
     </div>
     <div v-else-if="step === 3">
       <!-- Etapa 3: Dados de Pagamento -->
@@ -76,8 +78,6 @@
         <div>
           <h4>QR Code Pix:</h4>
           <QRCode :value="payloadGenerated" />
-          
-
         </div>
       </div>
       <button @click="finalizeOrder">Finalizar</button>
@@ -89,13 +89,14 @@
 import PayloadGenerator from "./Pix/PayloadGenerator";
 import QRCode from "./Qrcode/QRCode.vue";
 import Cookies from "js-cookie";
+import jwtDecode from "jwt-decode";
+import axios from "axios";
 
 export default {
   nome: "CheckOut",
   components: {
     QRCode,
   },
- 
 
   data() {
     return {
@@ -110,6 +111,7 @@ export default {
         email: "",
         formaPagamento: "",
       },
+      jwtDecoded: null,
     };
   },
   created() {
@@ -118,21 +120,89 @@ export default {
     if (cartData) {
       this.cart = JSON.parse(cartData); // Converta o valor do cookie em um objeto JSON
     }
+
+    const cartDataJSON = Cookies.get("cart");
+    
+  if (cartDataJSON) {
+    const cartData = JSON.parse(cartDataJSON);
+
+    // Extrai apenas os campos desejados e cria um novo array
+    const cartBuyData = cartData.map(item => {
+      return {
+        productId: item.id,
+        price: item.price,
+        amount: item.quantity,
+      };
+    });
+
+    // Salva os dados do carrinho de compra no cookie "cartbuy"
+    Cookies.set("cartbuy", JSON.stringify(cartBuyData));
+  } else {
+    // Se não houver dados no cookie "cart", crie um cookie "cartbuy" vazio
+    Cookies.set("cartbuy", "[]");
+  }
+    const jwtCookie = this.getCookie('authToken');
+
+// Decode JWT token
+    if (jwtCookie) {
+      try {
+        const decodedToken = jwtDecode(jwtCookie);
+
+        if (decodedToken) {
+          this.jwtDecoded = decodedToken;
+          // Acesse o campo correto do JWT para obter o ID do usuário
+          const userId = decodedToken.Id; // Substitua 'sub' pelo campo correto em seu JWT
+          console.log("Token decodificado:", decodedToken);
+          // Faça uma solicitação Axios para buscar os dados do usuário
+          axios
+            .get(`https://localhost:3000/Users/${userId}`)
+            .then((response) => {
+              // Preencha os dados do usuário no formulário com base na resposta
+              this.cliente = response.data;
+            })
+            .catch((error) => {
+              console.error("Erro ao buscar os dados do usuário:", error);
+            });
+        }
+      } catch (error) {
+        console.error("Erro ao decodificar o JWT:", error);
+      }
+    }
   },
   methods: {
+    getCookie(name) {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop().split(";").shift();
+    },
+
+
+
     generatePayloadAndProceedToNextStep() {
-      // Chame o gerador de payload com os dados do cliente
-      const payloadGenerator = new PayloadGenerator();
-      payloadGenerator.setAmount(this.calculateTotal()); // Defina o valor de amount corretamente
-      // Configure os dados do cliente aqui
-      const payload = payloadGenerator.generatePayload(); // Use o método do gerador
+  // Chame o gerador de payload com os dados do cliente
+  const payloadGenerator = new PayloadGenerator();
+  payloadGenerator.setAmount(this.calculateTotal()); // Defina o valor de amount corretamente
+  // Configure os dados do cliente aqui
+  const payload = payloadGenerator.generatePayload(); // Use o método do gerador
 
-      // Exiba a payload gerada
-      this.payloadGenerated = payload;
+  // Exiba a payload gerada
+  this.payloadGenerated = payload;
 
+  // Em seguida, faça a solicitação PUT para salvar os dados do cliente no banco de dados
+  axios.put(`https://localhost:3000/Users/${this.jwtDecoded.Id}`, this.cliente)
+    .then(response => {
+      // A solicitação foi bem-sucedida, você pode lidar com a resposta (opcional)
+      console.log('Dados do cliente salvos com sucesso:', response.data);
+      
       // Avance para a etapa 3 (Dados de Pagamento)
       this.step = 3;
-    },
+    })
+    .catch(error => {
+      console.error('Erro ao salvar os dados do cliente:', error);
+      // Lidar com erros de forma apropriada (exibindo uma mensagem de erro, revertendo alterações, etc.)
+    });
+
+},
     // Decrementa a quantidade de um item no carrinho
     decrementQuantity(item) {
       if (item.quantity > 1) {
@@ -142,26 +212,55 @@ export default {
         this.removeItemFromCart(this.cart.indexOf(item));
       }
     },
+
+
+
+    // Decrementa a quantidade de um item no carrinho
+    decrementQuantity(item) {
+      if (item.quantity > 1) {
+        item.quantity--;
+        this.updateCartCookie();
+      } else {
+        this.removeItemFromCart(this.cart.indexOf(item));
+      }
+    },
+
+
+
     // Incrementa a quantidade de um item no carrinho
     incrementQuantity(item) {
       item.quantity++;
       this.updateCartCookie();
     },
+
+
+
     // Remove um item específico do carrinho com base no índice
     removeItemFromCart(index) {
       this.cart.splice(index, 1);
       this.updateCartCookie();
     },
+
+
     getCartCookieName() {
       return Cookies.get("cart");
     },
+
+
+
     updateCartCookie() {
       const cartData = JSON.stringify(this.cart);
       Cookies.set("cart", cartData);
     },
+
+
+
     created() {
       this.getCartProductsFromCookies();
     },
+
+
+
     // Calcula o valor total do carrinho
     calculateTotal() {
       return this.cart.reduce(
@@ -169,15 +268,77 @@ export default {
         0
       );
     },
+
+
+    prevStep() {
+      this.step--;
+    },
+
+
+
+   async createPurchase() {
+  try {
+    // Decodifique o token JWT para obter o ID do usuário
+    const jwtDecoded = jwtDecode(this.getCookie("authToken"));
+    const userId = jwtDecoded.Id;
+
+    // Construa o objeto de compra com os dados fornecidos
+    const purchaseData = {
+      buyDate: "", // Use a data atual
+      payment: "", // Substitua pelo método de pagamento desejado
+      status: "Iniciado",
+      userId: userId, // Use o ID do usuário obtido do JWT
+      cartItems: [],
+    };
+
+    // Faça uma solicitação POST para criar a compra
+    const response = await axios.post(
+      "https://localhost:3000/Purchase",
+      purchaseData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    // Verifique se a resposta contém o ID da compra
+    const purchaseId = response.data.id; // Substitua 'Id' pelo campo correto em sua resposta JSON
+    
+
+    // Avance para a próxima etapa após o sucesso
+    this.nextStep();
+
+    // Agora, adicione o purchaseId aos cookies 'cartbuy'
+    const existingData = Cookies.get("cartbuy");
+
+    // Verifique se já há dados nos cookies 'cartbuy'
+    let cartBuyData = existingData ? JSON.parse(existingData) : [];
+
+    // Adicione o novo elemento 'purchaseId' ao array
+    cartBuyData.push({ purchaseId });
+
+    // Salve os dados atualizados nos cookies 'cartbuy'
+    Cookies.set("cartbuy", JSON.stringify(cartBuyData));
+    console.log('cartBuyData:', cartBuyData);
+  } catch (error) {
+    console.error("Erro ao registrar:", error);
+    // Trate o erro adequadamente aqui, você pode exibir uma mensagem de erro para o usuário ou fazer outras ações necessárias.
+  }
+},
+
+
+
+
     nextStep() {
       this.step++;
     },
     finalizeOrder() {
       // Limpar os dados do carrinho (cart) dos cookies
-      Cookies.remove('cart');
+      Cookies.remove("cart");
 
       // Redirecionar para a página "/home"
-      this.$router.push('/');
+      this.$router.push("/");
     },
   },
 };
